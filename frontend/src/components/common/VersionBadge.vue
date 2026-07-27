b74b6831dd3a76b6de19277739661f264c68eb29
kernelx30 <kernelx30@users.noreply.github.com>
2026-07-27 15:47:27 +0800

    chore: route updater to fork releases

--- frontend/src/components/common/VersionBadge.vue
+++ frontend/src/components/common/VersionBadge.vue
@@ -651,9 +651,9 @@ import {
 import { useClipboard } from '@/composables/useClipboard'
 import Icon from '@/components/icons/Icon.vue'
 
-const GITHUB_REPO = 'Wei-Shaw/sub2api'
-// Docker Hub image published by CI (tags carry no "v" prefix, e.g. weishaw/sub2api:0.1.146)
-const DOCKER_IMAGE = 'weishaw/sub2api'
+const GITHUB_REPO = 'kernelx30/sub2api'
+// Fork Docker Hub image (tags carry no "v" prefix, e.g. kernelx30/sub2api:0.1.146)
+const DOCKER_IMAGE = 'kernelx30/sub2api'
 
 const { t } = useI18n()
 
